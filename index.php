9e8712cbdbc9138458ce9b45b5fec74fefa65eeb
kenziajavasniscalaputraa <javastelkomschool@gmail.com>
2026-01-05 20:23:49 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple PHP file to test Git remote repository functionality.</p>
+</body>
+</html>
